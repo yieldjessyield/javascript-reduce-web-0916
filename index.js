bc4cc03697c0d7d38fcd2c1de574e6e33de9e055
Jess <jess.harrelson0707@gmail.com>
2016-11-07 09:32:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const app = "I don't do much."
+
+const products = [
+  { name: 'Head & Shoulders Shampoo', standardPrice: 4.99, discount: .6 },
+  { name: 'Twinkies', standardPrice: 7.99, discount: .45 },
+  { name: 'Oreos', standardPrice: 6.49, discount: .8 },
+  { name: 'Jasmine-scented bath pearls', standardPrice: 13.99, discount: .7 },
+];
+
+
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
+
+function callback(totalPrice, product) {
+  if (product.price < 7) {
+    return totalPrice + product.price;
+  }
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products, callback, 0));
+
+
+
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(reduce(couponLocations, couponCounter, 0));
